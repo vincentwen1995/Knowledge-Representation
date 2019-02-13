5aa0176ef03c297e5eb7a0cfc4b9e25e2768207e
Vincent Wen <x.wen@student.tue.nl>
2019-02-13 16:00:56 +0100

    Some updates.

--- week1/utils.py
+++ week1/utils.py
@@ -17,8 +17,13 @@ class Reader(object):
         self.file = file
 
     def read(self):
-        # with open(self.file, 'r') as input:
-        #     for line in input:
-        #         parsed = line.split()
-        #         if parsed[0] == 'p':
-        pass
+        with open(self.file, 'r') as input:
+            for line in input:
+                parsed = line.split()
+                if parsed[0] == 'p':
+                    var_cnts = int(parsed[2])
+                    self.vars = [False for i in range(var_cnts)]
+                elif parsed[0] == 'c':
+                    continue
+                else:
+                    pass
