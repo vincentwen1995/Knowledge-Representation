e3e698551ad6e854cb9355c518c638bcc520f99e
Vincent Wen <x.wen@student.tue.nl>
2019-02-13 13:39:43 +0100

    Bases.

--- SAT/SAT.py
+++ SAT/SAT.py
@@ -1,1 +1,13 @@
-# SAT solver
+#!/usr/bin/python
+# # SAT solver script
+
+import sys
+from utils import Reader, Clause
+
+
+def main(argv):
+    reader = Reader(argv[1])
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ SAT/utils.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+
+class Clause(object):
+    '''Class for representing a clause.
+    '''
+
+    def __init__(self):
+        pass
+
+
+class Reader(object):
+    '''Class for reading the input file.
+    '''
+
+    def __init__(self, file):
+        self.file = file
+
+    def read(self):
+        # with open(self.file, 'r') as input:
+        #     for line in input:
+        #         parsed = line.split()
+        #         if parsed[0] == 'p':
+        pass
