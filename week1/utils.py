ba40b530a78fdbfd868beae86b8459bcf3f4dd77
Vincent Wen <x.wen@student.tue.nl>
2019-02-21 17:41:22 +0100

    Rearranging.

--- week1/utils.py
+++ week1/utils.py
@@ -7,151 +7,170 @@ class Solver(object):
 
     def __init__(self, file):
         self.file = file
+        self.clauses = []
+
+    # def read(self):
+    #     '''Method for reading the clauses and do initial simplificaitons.
+    #     '''
+
+    #     # Initialize variables.
+    #     non_p_lits = []
+    #     vars_tmp = set()
+    #     immutable_vars = set()
+    #     self.p_lits = []
+    #     self.clauses = []
+
+    #     # Start reading from the file.
+    #     with open(self.file, 'r') as input_file:
+    #         for line in input_file:
+    #             parsed = line.split()
+
+    #             # Check whether it is valid line or supplementary line.
+    #             if parsed[0] == 'p' or parsed[0] == 'c':
+    #                 continue
+    #             else:
+    #                 eff_parsed = parsed[:-1]
+
+    #                 # Check for unit clauses.
+    #                 if len(eff_parsed) == 1:
+    #                     lit = eff_parsed[0]
+    #                     int_lit = int(lit)
+    #                     abs_lit = str(abs(int_lit))
+    #                     neg_lit = str(-int_lit)
+
+    #                     # Check if problem is solvable.
+    #                     if neg_lit in immutable_vars:
+    #                         self.output_result('no')
+
+    #                     # Collect immutable variables.
+    #                     immutable_vars.add(lit)
+
+    #                     # Collect variable.
+    #                     vars_tmp.add(abs_lit)
+
+    #                     # Check for pure literals.
+    #                     if neg_lit not in self.p_lits:
+    #                         if lit not in self.p_lits and abs_lit not in non_p_lits:
+    #                             self.p_lits.append(lit)
+    #                     else:
+    #                         self.p_lits.remove(neg_lit)
+    #                         non_p_lits.append(abs_lit)
+    #                 else:
+    #                     self.clauses.append(list())
+    #                     clause = self.clauses[-1]
+    #                     tautology = False
+    #                     for lit in eff_parsed:
+    #                         int_lit = int(lit)
+    #                         clause.append(lit)
+
+    #                         # Collect variable.
+    #                         abs_lit = str(abs(int_lit))
+    #                         vars_tmp.add(abs_lit)
+
+    #                         # Check for tautology.
+    #                         neg_lit = str(-int_lit)
+    #                         if neg_lit in clause:
+    #                             tautology = True
+
+    #                         # Check for pure literals.
+    #                         if neg_lit not in self.p_lits:
+    #                             if lit not in self.p_lits and abs_lit not in non_p_lits:
+    #                                 self.p_lits.append(lit)
+    #                         else:
+    #                             self.p_lits.remove(neg_lit)
+    #                             non_p_lits.append(abs_lit)
+
+    #                     # Remove clauses with tautology.
+    #                     if tautology:
+    #                         self.clauses.pop()
+
+    #     # Initialize all collected variables, e.g. {'115': [0, False] ...} - where [truth_val, mutability]
+    #     self.vars = dict.fromkeys(vars_tmp, [0, True])
+    #     self.imvars = immutable_vars
+    #     for var in immutable_vars:
+    #         int_var = int(var)
+    #         var_ind = str(abs(int_var))
+    #         if int_var < 0:
+    #             self.vars[var_ind] = [-1, False]
+    #         else:
+    #             self.vars[var_ind] = [1, False]
+    #     for var in self.p_lits:
+    #         int_var = int(var)
+    #         var_ind = str(abs(int_var))
+    #         if int_var < 0:
+    #             self.vars[var_ind] = [-1, False]
+    #         else:
+    #             self.vars[var_ind] = [1, False]
 
     def read(self):
         '''Method for reading the clauses and do initial simplificaitons.
         '''
-        
+
         # Initialize variables.
-        non_p_lits = []
         vars_tmp = set()
-        immutable_vars = set()
-        self.p_lits = []
-        self.clauses = []
-        
+        clauses = set()
         # Start reading from the file.
         with open(self.file, 'r') as input_file:
             for line in input_file:
                 parsed = line.split()
-                
+
                 # Check whether it is valid line or supplementary line.
                 if parsed[0] == 'p' or parsed[0] == 'c':
                     continue
                 else:
                     eff_parsed = parsed[:-1]
-                    
-                    # Check for unit clauses.
-                    if len(eff_parsed) == 1:
-                        lit = eff_parsed[0]
-                        int_lit = int(lit)
-                        abs_lit = str(abs(int_lit))
-                        neg_lit = str(-int_lit)
-                        
-                        # Check if problem is solvable.
-                        if neg_lit in immutable_vars:
-                            self.output_result('no')
-                        
-                        # Collect immutable variables.
-                        immutable_vars.add(lit)
-                        
+                    clause = set()
+                    for lit in eff_parsed:
+                        lit = int(lit)
+                        clause.add(lit)
+
                         # Collect variable.
+                        abs_lit = abs(lit)
                         vars_tmp.add(abs_lit)
-                        
-                        # Check for pure literals.
-                        if neg_lit not in self.p_lits:
-                            if lit not in self.p_lits and abs_lit not in non_p_lits:
-                                self.p_lits.append(lit)
-                        else:
-                            self.p_lits.remove(neg_lit)
-                            non_p_lits.append(abs_lit)
-                    else:
-                        self.clauses.append(list())
-                        clause = self.clauses[-1]
-                        tautology = False
-                        for lit in eff_parsed:
-                            int_lit = int(lit)
-                            clause.append(lit)
-                            
-                            # Collect variable.
-                            abs_lit = str(abs(int_lit))
-                            vars_tmp.add(abs_lit)
-                            
-                            # Check for tautology.
-                            neg_lit = str(-int_lit)
-                            if neg_lit in clause:
-                                tautology = True
-                            
-                            # Check for pure literals.
-                            if neg_lit not in self.p_lits:
-                                if lit not in self.p_lits and abs_lit not in non_p_lits:
-                                    self.p_lits.append(lit)
-                            else:
-                                self.p_lits.remove(neg_lit)
-                                non_p_lits.append(abs_lit)
-                        
-                        # Remove clauses with tautology.
-                        if tautology:
-                            self.clauses.pop()
-        
+                    clauses.add(list(clause))
+
         # Initialize all collected variables, e.g. {'115': [0, False] ...} - where [truth_val, mutability]
-        self.vars = dict.fromkeys(vars_tmp, [0, True])
-        self.imvars = immutable_vars
-        for var in immutable_vars:
-            int_var = int(var)
-            var_ind = str(abs(int_var))
-            if int_var < 0:
-                self.vars[var_ind] = [-1, False]
-            else:
-                self.vars[var_ind] = [1, False]
-        for var in self.p_lits:
-            int_var = int(var)
-            var_ind = str(abs(int_var))
-            if int_var < 0:
-                self.vars[var_ind] = [-1, False]
-            else:
-                self.vars[var_ind] = [1, False]
+        self.vars = dict.fromkeys(vars_tmp, False)
+        self.clauses = list(clauses)
 
-    def simplify(self, clauses):
-        clauses_tmp = []
-        for clause in clauses:
-            clause_tmp = []
+    def tautology(self):
+        '''Check and remove tautology.
+        '''
+
+        for clause in self.clauses:
+            if len(clause) == 1:
+                continue
+            else:
+                for lit in clause:
+                    if -lit in clause:
+                        self.clauses.remove(clause)
+                        break
+
+    def pure_literals(self):
+        '''Collect the pure literals.
+        '''
+
+        self.p_lits = []
+        non_p_lits = []
+        for clause in self.clauses:
             for lit in clause:
-                pass
+                neg_lit = -lit
+                abs_lit = abs(lit)
+                if neg_lit not in self.p_lits:
+                    if lit not in self.p_lits and abs_lit not in non_p_lits:
+                        self.p_lits.append(lit)
+                else:
+                    self.p_lits.remove(neg_lit)
+                    non_p_lits.append(abs_lit)
 
-    def evaluate_clause(self, clause):
-        '''Evaluate the truth of a clause.
-
-        Arguments:
-            clause {list} -- list of literals
-
-        Returns:
-            bool/str -- truth of the clause (or 'unk')
+    def unit_clauses(self):
+        '''Collect the variables in the unit clauses.
         '''
 
-        res = False
-        unks = 0
-        for lit in clause:
-            int_lit = int(lit)
-            if int_lit < 0:
-                val = -self.vars[str(abs(int_lit))][0]
-            else:
-                val = self.vars[str(abs(int_lit))][0]
-            if val > 0:
-                res = True
-                break
-            elif val == 0:
-                unks += 1
-        if unks > 0:
-            res = 'unk'
-        return res
-
-    def evalute(self, clauses):
-        '''Evaluate the problem.
-
-        Arguments:
-            clauses {list} -- list of clauses
-
-        Returns:
-            bool/str -- truth of the problem (or 'unk')
-        '''
-
-        res = []
-        for clause in clauses:
-            res_tmp = self.evaluate_clause(clause)
-            if res_tmp == 'unk':
-                return 'unk'
-            res.append(res_tmp)
-        return all(res)
+        self.unit_var = set()
+        for clause in self.clauses:
+            if len(clause) == 1:
+                self.unit_var.add(clause[0])
 
     def output_result(self, flag='yes'):
         '''Method for printing the final results.
@@ -164,10 +183,9 @@ class Solver(object):
             cnt = len(self.vars)
             print('p cnf {} {}'.format(cnt, cnt))
             for k in sorted(self.vars.iterkeys()):
-                if self.vars[k][0] < 0:
-                    print('-' + k + ' 0')
-                else:
+                if self.vars[k]:
                     print(k + ' 0')
+                else:
+                    print('-' + k + ' 0')
         else:
             print('The problem is unsolvable.')
-            exit()
