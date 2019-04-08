3b540bb53294d4305267475b30fd3a345d0c0839
Kai Liang <k.liang.2@student.rug.nl>
2019-04-08 18:53:36 +0200

    init. week2

--- week2/main.py
+++ week2/main.py
@@ -4,12 +4,17 @@ import sys
 quantity = dict()
 quantity['Inflow'] = dict()
 quantity['Inflow']['mag'] = '0'
+quantity['Inflow']['values'] = ['0', '+']
 quantity['Inflow']['de'] = '0'
 
 quantity['Volume'] = dict()
 quantity['Volume']['mag'] = '0'
+quantity['Inflow']['values'] = ['0', '+', 'max']
 quantity['Volume']['de'] = '0'
 
 quantity['Outflow'] = dict()
 quantity['Outflow']['mag'] = '0'
+quantity['Inflow']['values'] = ['0', '+', 'max']
 quantity['Outflow']['de'] = '0'
+
+print(quantity)
