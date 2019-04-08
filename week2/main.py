a618d6765f85c24a226e5a92e977b6aa5a8ff694
Kai Liang <k.liang.2@student.rug.nl>
2019-04-08 18:46:15 +0200

    init. week2

---
+++ week2/main.py
@@ -0,0 +1,15 @@
+import sys
+
+# intialize the scenario.
+quantity = dict()
+quantity['Inflow'] = dict()
+quantity['Inflow']['mag'] = '0'
+quantity['Inflow']['de'] = '0'
+
+quantity['Volume'] = dict()
+quantity['Volume']['mag'] = '0'
+quantity['Volume']['de'] = '0'
+
+quantity['Outflow'] = dict()
+quantity['Outflow']['mag'] = '0'
+quantity['Outflow']['de'] = '0'
