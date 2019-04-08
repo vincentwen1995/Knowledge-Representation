65dfeac654ac32c8903c93581e5db303ca9cdeb7
Kai Liang <k.liang.2@student.rug.nl>
2019-04-08 20:02:46 +0200

    update

--- week2/main.py
+++ week2/main.py
@@ -1,20 +1,34 @@
 import sys
 
 # intialize the scenario.
-quantity = dict()
-quantity['Inflow'] = dict()
-quantity['Inflow']['mag'] = '0'
-quantity['Inflow']['values'] = ['0', '+']
-quantity['Inflow']['de'] = '0'
+scenario = dict()
 
-quantity['Volume'] = dict()
-quantity['Volume']['mag'] = '0'
-quantity['Inflow']['values'] = ['0', '+', 'max']
-quantity['Volume']['de'] = '0'
+scenario['quantities'] = dict()
 
-quantity['Outflow'] = dict()
-quantity['Outflow']['mag'] = '0'
-quantity['Inflow']['values'] = ['0', '+', 'max']
-quantity['Outflow']['de'] = '0'
+scenario['quantities']['inflow'] = dict()
+scenario['quantities']['inflow']['mag'] = '+'
+scenario['quantities']['inflow']['space'] = ['0', '+']
+scenario['quantities']['inflow']['de'] = '0'
 
-print(quantity)
+scenario['quantities']['volume'] = dict()
+scenario['quantities']['volume']['mag'] = '0'
+scenario['quantities']['volume']['space'] = ['0', '+', 'max']
+scenario['quantities']['volume']['de'] = '0'
+
+scenario['quantities']['outflow'] = dict()
+scenario['quantities']['outflow']['mag'] = '0'
+scenario['quantities']['outflow']['space'] = ['0', '+', 'max']
+scenario['quantities']['outflow']['de'] = '0'
+
+scenario['influences'] = list()
+scenario['influences'].append(['inflow', 'volume', 1])
+scenario['influences'].append(['outflow', 'volume', -1])
+
+scenario['proportionalities'] = list()
+scenario['proportionalities'].append(['volume', 'outflow', 1])
+
+scenario['constraints'] = list()
+scenario['constraints'].append(['volume', 'outflow', 'max'])
+scenario['constraints'].append(['volume', 'outflow', '0'])
+
+print(scenario)
