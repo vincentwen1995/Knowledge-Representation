22e7ee56218918ba3af6f83b32c9be8ec522f512
Kai Liang <k.liang.2@student.rug.nl>
2019-04-12 12:19:33 +0200

    add printout

--- week2/main.py
+++ week2/main.py
@@ -4,6 +4,17 @@ import copy
 
 
 def main():
+    print('-----------------------------')
+    print('Assumptions:')
+    print('- Inflow behavior: positive parabola')
+    print('- Inflow intitial magnitude: 0')
+    print('- Inflow intitial derivative: +')
+    print('- Volume intitial magnitude: 0')
+    print('- Volume intitial derivative: 0')
+    print('- Outflow intitial magnitude: 0')
+    print('- Outflow intitial derivative: 0')
+    print('-----------------------------')
+    print('Applying Qualitative Reasoning...')
     scenario_quantities = (0, 1, 0, 0, 0, 0)
     scenario = State(1, 0, *scenario_quantities)
     solver = Flow(scenario)
@@ -12,7 +23,14 @@ def main():
     plotter.draw_states()
     plotter.output_graph()
     plotter.output_trace()
-    print('The total number of all states: {}'.format(len(result)))
+    print('-----------------------------')
+    print('Done!')
+    print('-----------------------------')
+    print('The total number of states: {}'.format(len(result)))
+    print('-----------------------------')
+    print('The state graph is exported to ./result/state_graph.pdf')
+    print('The trace file is exported to ./result/trace.txt')
+    print('-----------------------------')
 
 
 if __name__ == '__main__':
